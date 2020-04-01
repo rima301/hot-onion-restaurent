5101e0315cf918b287d2e9cfdb84aaedbd7b2a81
rima301 <rima301@gmail.com>
2020-04-02 00:15:47 +0800

    2nd commit

--- src/components/Breakfast/Breakfast.js
+++ src/components/Breakfast/Breakfast.js
@@ -6,13 +6,21 @@ import fakeData from '../../fakeData'
 const Breakfast = () => {
     const [items, setitems] =  useState(fakeData);
     const breakfast = items.filter(item => item.category === "breakfast");
+    
+    const handleAddItem = () => {
+        console.log("item added")
+
+    }
     return (
         <div className="row">
             
         
         {
             breakfast.map( item => (<div className="col-md-4">
-            <Items item={item}></Items></div>))
+            <Items 
+                handleAddItem = {handleAddItem}
+                item={item}>
+            </Items></div>))
         }
         </div>
     );
--- src/components/FoodDetails/FoodDetails.js
+++ src/components/FoodDetails/FoodDetails.js
@@ -4,11 +4,15 @@ import fakeData from '../../fakeData';
 import Items from '../Items/Items';
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row';
+import './FoodDetails.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartArrowDown, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
 
 
 
-const FoodDetails = () => {
+
+const FoodDetails = (props) => {
      const {itemId} = useParams();
      const food = fakeData.find(idValue => idValue.id == itemId);
      
@@ -16,20 +20,26 @@ const FoodDetails = () => {
      console.log(food)
    
     return (
-        <div className="details-container">
+        <div >
 
             
-            <Row>
+            <Row className="details-container">
                 
                 
                 <Col xs lg="6">
                     <div className="food-container"  >
                         <h1>{food.name}</h1>
                         <p> <small>{food.description}</small> </p>
-                        <h3>${food.price}</h3>
-                        <button>btn1</button>
-                        <br/>
-                        <button>btn2</button>
+                        <div className="flex-container">
+                            <div className="margin-container"><h3>${food.price}</h3> </div>
+                            <button> <FontAwesomeIcon icon={faPlus}/>  </button>
+                            <button> <FontAwesomeIcon icon={faMinus}/> </button>
+
+                        </div>
+                        <div >
+                            <button className="btn-container" onClick={props.handleAddItem}> <FontAwesomeIcon icon={faCartArrowDown}/> Add </button>
+                        </div>
+                        
                     </div>
                 </Col>
                 
